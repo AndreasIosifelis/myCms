c432fe981a09ab5463b7d747abeb05ff32440209
drumforhim <drumforhim@gmail.com>
2014-06-25 01:44:08 +0300

    first

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost:'.$_SERVER['SERVER_PORT'].'/myCms/';
+$config['base_url']	= 'http://localhost:'.$_SERVER['SERVER_PORT'].'/myCms/myCms/';
 
 /*
 |--------------------------------------------------------------------------
